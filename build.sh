e07ff5a96d97ad492612985ef5e736dde0160c75
sokhevinoem <sokhevinoem@gmail.com>
2026-09-07 13:36:29 +0700

    Add auto-superuser creation script

--- build.sh
+++ build.sh
@@ -4,3 +4,12 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# បង្កើត Superuser ដោយស្វ័យប្រវត្តិ
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='kevin').exists():
+    User.objects.create_superuser('kevin', 'admin@example.com', 'Admin123')
+    print('Superuser created successfully!')
+"
